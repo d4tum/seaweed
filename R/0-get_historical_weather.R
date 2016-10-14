0339fa6865ef3bb13c8dd23e0320f91d2b6d2272
qubz <odbgza@gmail.com>
2016-10-15 10:06:42 +1100

    curl revert

--- R/0-get_historical_weather.R
+++ R/0-get_historical_weather.R
@@ -23,6 +23,7 @@
 #' @importFrom lubridate month
 #' @importFrom lubridate date
 #' @importFrom lubridate rollback
+#' @importFrom curl has_internet
 #' @importFrom weatherData getWeatherForDate
 get_weather <- function(m, download) {
   if (!download) {
@@ -35,7 +36,7 @@ get_weather <- function(m, download) {
     dt <- subset(dt, month(date(Date)) <= m)
     dt
 
-  } else if (curl::has_internet()) {
+  } else if (has_internet()) {
     # Download a fresh batch
     # Set up weather station ICAO codes and give them 3 letter IATA names
     station_codes <-
